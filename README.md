a50d20f79d243c2285de40ec8fcce80f1b410795
Sato <sera.martin@gmail.com>
2021-01-25 20:46:43 +0100

    Nivell2-Exercici1

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Asyn-Await Nivell1-Exercici2
+# Asyn-Await Nivell2-Exercici1
 
-Imprimimos el nombre y el sueldo.
+Función async que llama a otra que devuelve una promesa que resuelve en 2 segundos. He pasado mucho rato dándole vueltas a por qué me saltaba el reject antes que el resolve (de superdotado: obviamente porque tardas 2 segundos en el resolve... o haces que el reject tarde más o te la tira) así que ya tiene hecha la recogida de errores que piden en el siguiente ejercicio.
--- logica.js
+++ logica.js
@@ -1,60 +1,24 @@
-let employees = [{
-    id: 1,
-    name: 'Linux Torvalds'
-}, {
-    id: 2,
-    name: 'Bill Gates'
-},{
-    id: 3,
-    name: 'Jeff Bezos'
-}];
- 
-let salaries = [{
-    id: 1,
-    salary: 4000
-}, {
-    id: 2,
-    salary: 1000
-}, {
-    id: 3,
-    salary: 2000
-}];
-
-let getEmpleado = (id)=>{
-	let empleado =new Promise((resolve,reject) =>{	
-		employees.forEach(employe=>{
-			if(employe.id==id){	
-				resolve(employe);				
-			}
-		});		
-		reject(new Error("El id no aparece en el registro de empleados"));	
-	});
-	return empleado;
+async function yaCasiEsta(){
+	let estado = new Promise((resolve,reject) =>{	
+		setTimeout(() => {
+			resolve("Completado!");
+		}, 2000);		
+		setTimeout(() => {
+			reject(new Error("No dio tiempo!"));
+		}, 2500);
+	});	
+	return estado;
 }
 
-let getSalario =(empleado)=>{
-	let salario = new Promise((resolve,reject)=>{
-		salaries.forEach(mount=>{
-			if(mount.id==empleado.id){
-					resolve(mount.salary);
-			}
-		});
-		reject(new Error("Esta personita no cobra"));	
-	});
-	return salario;
-}
-
-
-async function imprimePersonaSalario(id){
-	let persona="";
-	try{
-		persona = await getEmpleado(id);
-		salario = await getSalario(persona);
-		console.log(`La persona llamada ${persona.name} cobra ${salario}.`);			
-	}catch (err) {
+async function compruebaSiYa(){
+	try{		
+		let respuesta  = await yaCasiEsta();
+		console.log(respuesta);
+	}catch (err){
 		console.log(err.message);
 	}
-				
 }
 
-imprimePersonaSalario(1);
+compruebaSiYa();
+
+
